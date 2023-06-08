7fc8f57f4aa8211724f09117d0224e57e702a68d
KidneyDoctor <133861202+KidneyDoctor@users.noreply.github.com>
2023-06-08 12:33:06 +0530

    first commit

---
+++ Gredy.py
@@ -0,0 +1,14 @@
+#List
+list = [1,2,3,"Python", 2.3]
+print(list)
+#Creating a dictionary
+dict = {'Name':'Python','Num':[1,2,3,4]}
+print("Creating a dictionary")
+print(dict)
+#tuple
+tup=(1,"a","tuple",2)
+print(tup)
+#Sets
+Set = set([1,2,'Sets',4,'in',6,'Python'])
+print("set with the use of mixed value")
+print(set)
